e895cd8d1e7a5cae8739e538fe99bdd7961fa1bf
DandelionJR <luyongjie1115@163.com>
2018-03-28 09:38:17 +0800

    解释器模式

---
+++ src/interpreterPattern/client1/AbstractNode.java
@@ -0,0 +1,6 @@
+package interpreterPattern.client1;
+
+//抽象表达式
+public abstract class AbstractNode {
+    public abstract String interpret();
+}
---
+++ src/interpreterPattern/client1/DistanceNode.java
@@ -0,0 +1,14 @@
+package interpreterPattern.client1;
+
+//距离解释：终结符表达式
+public class DistanceNode extends AbstractNode {
+    private String distance;
+    public DistanceNode(String distance) {
+        this.distance = distance;
+    }
+    //距离表达式的解释操作
+    public String interpret() {
+        return this.distance;
+    }
+
+}
