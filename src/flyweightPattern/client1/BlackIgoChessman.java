3e965499221ead6c423063e64b869b934c74273c
DandelionJR <luyongjie1115@163.com>
2018-03-28 09:37:27 +0800

    享元模式

---
+++ src/flyweightPattern/client1/BlackIgoChessman.java
@@ -0,0 +1,8 @@
+package flyweightPattern.client1;
+
+//黑色棋子类：具体享元类
+public class BlackIgoChessman extends IgoChessman{
+    public String getColor() {
+        return "黑色";
+    }
+}
---
+++ src/flyweightPattern/client1/WhiteIgoChessman.java
@@ -0,0 +1,8 @@
+package flyweightPattern.client1;
+
+//白色棋子类：具体享元类
+public class WhiteIgoChessman extends IgoChessman {
+    public String getColor() {
+        return "白色";
+    }
+}
